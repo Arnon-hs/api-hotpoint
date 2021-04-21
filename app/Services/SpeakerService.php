c6297a8ec000f0c1a31376c584bff5661aa55f92
Vasiliy Bereznikov <v_bereznikov@proofix.ru>
2021-04-21 12:44:11 +0300

    delete some shit

--- app/Services/SpeakerService.php
+++ app/Services/SpeakerService.php
@@ -17,23 +17,7 @@ class SpeakerService
     public function getSpeakers()
     {
         try {
-//            if($result = app('redis')->get('speakers'))
-//                return $result;
             $result = $this->speakerRepository->getSpeakers();
-            foreach ($result as &$speaker) {
-                $speaker['name'] = $speaker['speaker_fname'].' '.$speaker['speaker_lname'];
-                $speaker['position'] = $speaker['speaker_titles'];
-                $speaker['company'] = $speaker['speaker_companies'];
-                $speaker['photo'] = $speaker['speaker_image'];
-                unset($speaker['speaker_lname'],
-                    $speaker['speaker_fname'],
-                    $speaker['speaker_mname'],
-                    $speaker['questionid'],
-                    $speaker['speaker_titles'],
-                    $speaker['speaker_companies'],
-                    $speaker['speaker_image']);
-            }
-//            app('redis')->set('speakers', $result);
         } catch (\Exception $e) {
             Log::error($e->getMessage());
             throw new InvalidArgumentException('Unable get speakers');
