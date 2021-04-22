5984456d91121377a7acfc132bd88a2cca39b0a8
Pavel Smyshlyev <ps@proofix.ru>
2021-04-22 07:04:55 +0200

    P.Smyshlyev: update respons arr for SessionService/getSessions

--- app/Services/SessionService.php
+++ app/Services/SessionService.php
@@ -19,21 +19,49 @@ class SessionService
     public function getSessions()//TODO
     {
         $sessions = $this->sessionRepository->getSessions();
-        $result = [];
+        $result = [
+            [],
+            [],
+            []
+        ];
         foreach ($sessions as $key => $session) {
-            if (!isset($result[$session->sessiondate]))
-                $result[$session->sessiondate] = [];
+            $names = [];
+
+            $location_name = Location::where('location_id',$session->location_id)->get()->first()->name;
+            $explode_speakers = explode(';' ,$session->speaker_ids);
+
+            DB::table('speakers')->whereIn('speaker_id', $explode_speakers)->get()->each(function ($speaker_name) use (&$names){
+                $names[] = $speaker_name->name;
+            });
 
             $start_time_ex = substr($session->starttime, 0, -3);
             $end_time_ex = substr($session->endtime, 0, -3);
-            $result[$session->sessiondate][$key]['time'] = $start_time_ex . ' - ' . $end_time_ex;
-            $result[$session->sessiondate][$key]['header'] = "Пленарная сессия";
-            $result[$session->sessiondate][$key]['title'] = "Сервис в эпоху цифровизации";
-            $result[$session->sessiondate][$key]['speakers'] = "Иван Иванов";
-            $result[$session->sessiondate][$key]['streamId'] = 1;
-            unset($session->starttime);
-            unset($session->endtime);
-            unset($session->sort);
+            switch ($session->sessiondate) {
+                case '2021-04-27':
+                    $result[0][] = [
+                        'title' => $session->name,
+                        'time' => $start_time_ex . ' - ' . $end_time_ex,
+                        'location' => $location_name,
+                        'speakers' => implode(", " , $names)
+                    ];
+                    break;
+                case '2021-04-28':
+                    $result[1][] = [
+                        'title' => $session->name,
+                        'time' => $start_time_ex . ' - ' . $end_time_ex,
+                        'location' => $location_name,
+                        'speakers' => implode(", " , $names)
+                    ];
+                    break;
+                case '2021-04-29':
+                    $result[2][] = [
+                        'title' => $session->name,
+                        'time' => $start_time_ex . ' - ' . $end_time_ex,
+                        'location' => $location_name,
+                        'speakers' => implode(", " , $names)
+                    ];
+                    break;
+            }
         }
         return $result;
     }
